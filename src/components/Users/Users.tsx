f704e799e9a7c4b9525b85440fcd4bbf56c904ab
alex-machulski <aliaksandr.machulski@gmail.com>
2021-02-05 20:48:26 +0300

    added Users' server request in componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -13,13 +13,10 @@ type UsersPropsType = {
 
 class Users extends React.Component<UsersPropsType, {}> {
 
-    constructor(props: UsersPropsType) {
-        super(props);
-        if (this.props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                this.props.setUsers(response.data.items)
-            })
-        }
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        })
     }
 
     render() {
